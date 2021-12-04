53045d024c7c9616526d4655ae84ead0eca49b33
Jawee <miyoriohara@gmail.com>
2021-12-04 19:27:21 +0800

    make it better

---
+++ resources/views/home.blade.php
@@ -0,0 +1,75 @@
+div class="header">
+  <a href="#default" class="logo">Info Center</a>
+  <div class="header-right">
+    <a class="active" href="index.blade.php">Home</a>
+    <a href="entertainmentnewscontent">Entertainment</a>
+    <a href="promotionalbanner">Promotional</a>
+    <a href="tabloidcontent">Tabloid</a>
+    <a href="adsbanner">ads</a>
+
+  </div>
+</div>
+
+<link rel="stylesheet" type="text/css" href="{{URL('/css/index.css')}}">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+
+body { 
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+}
+
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+@media screen and (max-w  idth: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  
+  .header-right {
+    float: none;
+  }
+}
+</style>
+</head>
+<body>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\fortnite;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
     
 });
 
